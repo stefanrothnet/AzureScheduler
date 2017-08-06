2ed6b7a2dd3afd6665965ea4f2d5be6d8b4443a0
Stefan Roth <returnone@outlook.com>
2017-08-06 19:38:25 +0200

    Update Get-SchedulerJob.ps1

--- scripts/Get-SchedulerJob.ps1
+++ scripts/Get-SchedulerJob.ps1
@@ -1,1 +1,47 @@
-Write-Warning "Test"
+$LogType = "AzureScheduler"
+$TimeStampField=""
+$RunTime = [System.DateTime]::UtcNow
+
+$CustomerID = Get-AutomationVariable -Name "OMS Workspace ID"
+$SharedKey =  Get-AutomationVariable -Name "OMS Workspace Key"
+$Subscription = Get-AutomationVariable -Name "Subscription"
+$Credential = Get-AutomationPSCredential -Name 'AzureSchedulerCredential'
+
+Add-AzureRMAccount -Credential $Credential  -Subscriptionid $Subscription
+
+$JobData = Get-AzureRmSchedulerJobCollection | Get-AzureRmSchedulerJob 
+
+If($JobData)
+{
+    $JobCollection = @()
+    ForEach ($Job in $JobData)
+    {
+       $Job | Add-Member -MemberType NoteProperty -Name 'RunTime' -Value $RunTime
+       $Jobcollection += $Job
+    } 
+
+    $Body = ConvertTo-Json  $JobCollection 
+
+    try {
+
+        Send-OMSAPIIngestionFile -customerId $CustomerID -sharedKey $SharedKey -body $Body -logType $LogType -TimeStampField $TimeStampField
+        $Body
+        Write-Information "Successfully sent job data to OMS"
+        }
+
+    Catch
+
+    {
+
+        $ErrorMessage = $_.Exception.Message
+
+	    Write-Error "Error submitting data: $ErrorMessage"
+
+        Exit
+
+    }
+}
+Else
+{
+    Write-Information "No Azure Scheduler data available."
+}
